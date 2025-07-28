13f4a86bd764ce6e912b2e4ef1b3a8555b400db7
thiago rabaioli <tfrabaioli@outlook.com>
2025-07-28 08:35:20 +0100

    First commit

---
+++ src/test/java/tfr/LostAndFoundAPP/LostAndFoundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tfr.LostAndFoundAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
