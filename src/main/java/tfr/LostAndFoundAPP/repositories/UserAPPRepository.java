edd7f90f27f8d974e66e7b07308d38ed1eca8bbd
thiago rabaioli <tfrabaioli@outlook.com>
2025-07-30 13:48:32 +0100

    Fase 2 - Security config - Oauth2 - part 2 - tested Login POstman OK

--- src/main/java/tfr/LostAndFoundAPP/repositories/UserAPPRepository.java
+++ src/main/java/tfr/LostAndFoundAPP/repositories/UserAPPRepository.java
@@ -11,14 +11,14 @@ import java.util.List;
 @Repository
 public interface UserAPPRepository  extends JpaRepository<UserAPP,Long> {
 
-    @Query(nativeQuery = true, value = """
-			SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
-			FROM tb_user
-			INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
-			INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
-			WHERE tb_user.email = :email
-		""")
-    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+	@Query(nativeQuery = true, value = """
+		SELECT tb_user_app.email AS username, tb_user_app.password, tb_role.id AS roleId, tb_role.authority
+		FROM tb_user_app
+		INNER JOIN tb_user_role ON tb_user_app.id = tb_user_role.user_id
+		INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+		WHERE tb_user_app.email = :email
+	""")
+	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 
 }
 
